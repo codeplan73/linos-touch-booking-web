b31734c876c19d1d4c96cbc797dfe493fb0d30b9
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2024-03-19 15:20:32 +0100

    completed homepage testimonial section

--- app/components/home/Testimonials.tsx
+++ app/components/home/Testimonials.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Testimonials = () => {
   return (
-    <div className="w-full md:max-w-7xl flex flex-col items-center justify-center md:gap-20">
+    <div className="w-full md:max-w-7xl flex flex-col items-center justify-center gap-10 md:gap-20">
       <div className="w-full flex flex-col md:flex-row items-center justify-between gap-4">
         <div className="flex flex-col items-center md:items-start justify-start gap-4">
           <p className="text-md font-sans uppercase font-light tracking-widest">
@@ -63,6 +63,13 @@ const Testimonials = () => {
           title="Installation of new electrical outlet"
         />
       </div>
+
+      <Link
+        href="/testimonial"
+        className="block md:hidden bg-warningColor rounded-full py-3 px-6 text-secondaryColor text-sm font-semibold font-sans mt-12"
+      >
+        See All Testimonial
+      </Link>
     </div>
   );
 };
