2e1c4656915e8a4cfd2e261ce6064981588c951a
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2024-03-19 15:17:28 +0100

    completed testimonial card

--- app/components/home/Testimonials.tsx
+++ app/components/home/Testimonials.tsx
@@ -1,9 +1,10 @@
+import TestimonialCard from "@/app/testimonial/_components/TestimonialCard";
 import Link from "next/link";
 import React from "react";
 
 const Testimonials = () => {
   return (
-    <div className="w-full md:max-w-7xl flex flex-col items-center justify-center gap-12">
+    <div className="w-full md:max-w-7xl flex flex-col items-center justify-center md:gap-20">
       <div className="w-full flex flex-col md:flex-row items-center justify-between gap-4">
         <div className="flex flex-col items-center md:items-start justify-start gap-4">
           <p className="text-md font-sans uppercase font-light tracking-widest">
@@ -24,7 +25,44 @@ const Testimonials = () => {
         </Link>
       </div>
 
-      <div></div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+        <TestimonialCard
+          name="Walter Anderson"
+          rating="4.5/5"
+          text=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, sequi."
+          title="Faulty Wiring in my Kitcjen"
+        />
+        <TestimonialCard
+          name="Susan Torres"
+          rating="4.5/5"
+          text=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, sequi."
+          title="Circuit Breaker Tripping"
+        />
+        <TestimonialCard
+          name="John Davis"
+          rating="4.5/5"
+          text=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, sequi."
+          title="Flickering Lights in The Living Room"
+        />
+        <TestimonialCard
+          name="Melisa Rose"
+          rating="4.5/5"
+          text=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, sequi."
+          title="Installation of new electrical outlet"
+        />
+        <TestimonialCard
+          name="Brandon Millet"
+          rating="4.5/5"
+          text=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, sequi."
+          title="Power Outage troubleshooting."
+        />
+        <TestimonialCard
+          name="Gloria Williams"
+          rating="4.5/5"
+          text=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, sequi."
+          title="Installation of new electrical outlet"
+        />
+      </div>
     </div>
   );
 };
--- app/testimonial/_components/TestimonialCard.tsx
+++ app/testimonial/_components/TestimonialCard.tsx
@@ -1,7 +1,48 @@
 import React from "react";
+import { FaStar } from "react-icons/fa";
+import { AiOutlineInfoCircle } from "react-icons/ai";
 
-const TestimonialCard = () => {
-  return <div>TestimonialCard</div>;
+interface Props {
+  name: string;
+  rating: string;
+  title: string;
+  text: string;
+}
+
+const TestimonialCard = ({ name, rating, title, text }: Props) => {
+  return (
+    <div className="flex flex-col gap-6 max-w-max-w-md bg-white rounded-2xl drop-shadow-2xl p-8 font-sans">
+      <div className="flex items-start justify-start gap-4">
+        <p className="bg-blue-500 h-20 w-20 rounded-2xl"></p>
+        <div className="flex flex-col items-start gap-4">
+          <h4 className="text-xl md:text-2xl font-semibold">{name}</h4>
+          <p className="flex items-center gap-2">
+            <FaStar className="text-primaryColor text-2xl" />
+            <span className="text-blue-500 font-light">{rating}</span>
+          </p>
+        </div>
+      </div>
+
+      <div className="flex flex-col items-start justify-start gap-4">
+        <div className="flex items-center gap-2 justify-start">
+          <span>
+            <AiOutlineInfoCircle className="text-red-500 text-xl" />
+          </span>
+          <h4 className="text-md font-semibold text-secondaryColor">{title}</h4>
+        </div>
+
+        <p className="text-md text-slate-400 text-left">{text}</p>
+        <p className="text-sm text-slate-400">
+          {new Date().toLocaleDateString("default", {
+            weekday: "long",
+            month: "long",
+            day: "numeric",
+          })}{" "}
+          {new Date().getFullYear()}
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default TestimonialCard;
