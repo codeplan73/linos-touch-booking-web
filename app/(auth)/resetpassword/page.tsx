deca22c1f9ad4b0df6770348aee13e3912c9b08b
Emmanuel Omonzebaguan <codeplan73@gmail.com>
2024-03-22 11:03:08 +0100

    build opt form reset

--- app/(auth)/resetpassword/page.tsx
+++ app/(auth)/resetpassword/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Image from "next/image";
-import OTPForm from "./forgotPasswordForm";
+import OTPForm from "./OTPForm";
 import Link from "next/link";
 import { IoIosArrowBack } from "react-icons/io";
 
@@ -25,13 +25,13 @@ const ResetPasswordPage = () => {
         </Link>
 
         <div className="flex flex-col gap-2">
-          <h4 className="text-left text-4xl font-bold text-black">
-            Forgot Password
-          </h4>
-          <p className="text-md text-left text-slate-600">{`Enter your registered email address, we'll send you a code to reset your password`}</p>
+          <h4 className="text-left text-4xl font-bold text-black">Enter OTP</h4>
+          <p className="text-md text-left text-slate-600">
+            We have sent an OTP to your registered email address email@gmail.com
+          </p>
         </div>
 
-        <ForgotPasswordForm />
+        <OTPForm />
       </div>
     </div>
   );
